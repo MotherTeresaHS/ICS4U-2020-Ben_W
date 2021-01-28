6b58355c67a04091cea28e8aeb51c3e1c3c4636f
ben-whitten <54954680+ben-whitten@users.noreply.github.com>
2021-01-28 16:37:54 -0500

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,4 +1,4 @@
-*
+/*
 * The dice program implements an application that
 * rolls dice.
 *
@@ -12,14 +12,16 @@ import java.util.Random; // Import the random number class
 
 public class Dice {
 
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-100.
    */
   public static int rollD100() {
     return (int) (Math.random() * 100 + 1);
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-20.
    */
@@ -43,47 +45,52 @@ public class Dice {
       return roll1;
     }
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-12.
    */
   public static int rollD12() {
     return (int) (Math.random() * 12 + 1);
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-10.
    */
   public static int rollD10() {
     return (int) (Math.random() * 10 + 1);
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-8.
    */
   public static int rollD8() {
     return (int) (Math.random() * 8 + 1);
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-6.
    */
   public static int rollD6() {
     return (int) (Math.random() * 6 + 1);
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-4.
    */
   public static int rollD4() {
     return (int) (Math.random() * 4 + 1);
   }
-//=============================================================================
+  //===========================================================================
+
   /**
    * This functions generates a random number from 1-3.
    */
   public static int rollD3() {
     return (int) (Math.random() * 3 + 1);
   }
-//=============================================================================
 }
